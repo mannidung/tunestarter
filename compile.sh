9fb6a4d9f29a57b77d775f8485e174d0f5239657
Pär Persson Mattsson <p.perssonmattsson@gmail.com>
2022-07-17 17:28:26 +0200

    Adds more sets and fixes some bugs

--- compile.sh
+++ compile.sh
@@ -8,17 +8,19 @@ cp -R $BOILERPLATE_DIR/* $TMP_COMPILE/
 
 #######
 
-FOLDERS=("sets/jigs")
-for FOLDER in $FOLDERS; do
+FOLDERS=("sets/jigs" "sets/reels")
+for FOLDER in ${FOLDERS[@]}; do
     cd $FOLDER
-    for SET in "$(ls)"; do
+    ls | while read SET; do
         #echo $SET
         cd "$SET"
         SETNAME_UNDERSCORE=${SET// /_}
         FILENAME=$SETNAME_UNDERSCORE.tex
+        #echo $SETNAME_UNDERSCORE
+        #echo $FILENAME
         echo "\subsection{$SET}" > $FILENAME
         COUNTER=1
-        for TUNE in $(ls *.abc); do
+        ls *.abc | while read TUNE; do
             #echo $COUNTER
             echo "\\begin{abc}[name=${SETNAME_UNDERSCORE}_${COUNTER}]" >> $FILENAME
             sed '/|/q' $TUNE >> $FILENAME
@@ -29,9 +31,10 @@ for FOLDER in $FOLDERS; do
         #echo $TMP_COMPILE/$FOLDER/$FILENAME
         #cat $FILENAME
         mv $FILENAME $ROOT/$TMP_COMPILE/$FOLDER/
-        echo "\\input{./sets/jigs/$FILENAME} \clearpage" >> $ROOT/$TMP_COMPILE/$FOLDER/00-Index.tex
+        echo "\\input{./${FOLDER}/$FILENAME} \clearpage" >> $ROOT/$TMP_COMPILE/$FOLDER/00-Index.tex
         #echo $ROOT/$TMP_COMPILE/$FOLDER/00-Index.tex
         cat $ROOT/$TMP_COMPILE/$FOLDER/00-Index.tex
+        cd ..
     done
     cd $ROOT
 done
@@ -39,9 +42,10 @@ done
 #######
 #echo "Exiting"
 #exit
-#echo "This should not show"
 
 cd $TMP_COMPILE
+# Repeate pdflatex twice to build index
+pdflatex  -synctex=1 -interaction=nonstopmode -file-line-error -recorder  -shell-escape "Tunestarter.tex" 
 pdflatex  -synctex=1 -interaction=nonstopmode -file-line-error -recorder  -shell-escape "Tunestarter.tex" 
 mv Tunestarter.pdf ../
 cd ..
